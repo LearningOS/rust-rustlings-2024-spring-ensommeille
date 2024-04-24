4aa987168c4c329b528af7f77f9137bcc97d32a2
CAN LIAO <117738933+ensommeille@users.noreply.github.com>
2024-04-24 13:45:19 +0000

    push

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut name = String::new();
+        let mut age = 0;
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2{
+            return Person::default();
+        }
+        name = parts[0].to_string();
+        if name.is_empty() {
+            return Person::default();
+        }
+        if parts.len() > 1 {
+            match parts[1].parse::<usize>() {
+                Ok(a) => age = a,
+                Err(_) => return Person::default(),
+            }
+        }
+        Person { name, age }
     }
 }
 
